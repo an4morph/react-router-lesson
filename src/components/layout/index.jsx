88ea3f495301ba39d8ab54894bb45f444d2942b5
an4morph <avgustin456@gmail.com>
2023-03-16 20:44:27 +0600

    add styles for layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import { Nav } from "../../components/nav"
 import { Footer } from "../footer"
+import './layout.css'
 
 export const Layout = () => {
   return (
